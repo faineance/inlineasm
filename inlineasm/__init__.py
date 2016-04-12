b38dc6cf0e83cffa1c971b7ab899dcfbc2cd9ef5
faineance <faineance@users.noreply.github.com>
2016-04-12 22:55:29 +0100

    use nasm to compile assembly

--- inlineasm/__init__.py
+++ inlineasm/__init__.py
@@ -1,15 +1,30 @@
 from ctypes import *
 from mmap import *
+import tempfile
+import subprocess
 
 libc = cdll.LoadLibrary("libc.so.6")
 mmap = libc.mmap
+import os
 
 munmap = libc.munmap
 munmap.argtype = [c_void_p, c_size_t]
 
 
 class assemble(object):
-    def __init__(self, code, ret_type, *arg_types):
+    def __init__(self, code, ret_type, *arg_types, nasm=False):
+        if nasm:
+            with tempfile.NamedTemporaryFile(suffix='.asm') as tmp:
+                tmp.write(str.encode(code))
+                tmp.flush()
+                output_path = "{}.bin".format(tmp.name)
+                subprocess.check_call(["nasm", tmp.name, "-o", output_path])
+
+                output = open(output_path, 'rb')
+                code = output.read()
+                output.close()
+                os.remove(output_path)
+
         if not isinstance(code, list) and isinstance(code, str):
             code = [ord(c) for c in code]
 
@@ -30,4 +45,3 @@ class assemble(object):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         munmap(self.mem, self.length)
-
